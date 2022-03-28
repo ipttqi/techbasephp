fa4621b8da23606deb2456839ce8a6b87ce0ab44
ipt_tqi <93820817+ipttqi@users.noreply.github.com>
2022-03-29 03:37:40 +0900

    Update mission4-8.php

--- mission4-8.php
+++ mission4-8.php
@@ -1,9 +1,9 @@
 <?php 
 
     // DB接続設定
-    $dsn = 'mysql:dbname=tb230896db;host=localhost';
-    $user = 'tb-230896';
-    $password = 'UJX8ThVnUS';
+    $dsn = 'mysql:dbname=***;host=localhost';
+    $user = '***';
+    $password = '***';
     $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
     $id = 2;
     $sql = 'delete from tbtest where id=:id';
