930a5f34f2f5d621d80e95605f952c3c38dbd2ad
ipt_tqi <93820817+ipttqi@users.noreply.github.com>
2022-03-29 03:32:47 +0900

    Update mission4-1.php

--- mission4-1.php
+++ mission4-1.php
@@ -1,8 +1,8 @@
 <?php 
 
     // DB接続設定
-    $dsn = 'mysql:dbname=tb230896db;host=localhost';
-    $user = 'tb-230896';
-    $password = 'UJX8ThVnUS';
+    $dsn = 'mysql:dbname=***;host=localhost';
+    $user = '***';
+    $password = '***';
     $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 ?>
